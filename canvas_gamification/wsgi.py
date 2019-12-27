0f1791f1ae3835c670276118663a8c60f1d6c4e7
keyvankhademi <keyvankhademi@gmail.com>
2019-12-27 20:03:26 +1100

    add django application

---
+++ canvas_gamification/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for canvas_gamification project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'canvas_gamification.settings')
+
+application = get_wsgi_application()
