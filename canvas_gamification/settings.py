81c8fba666a59876838bece592fc1be881a9c501
keyvankhademi <keyvankhademi@gmail.com>
2020-05-05 17:42:12 -0700

    add whitenoise middle ware

--- canvas_gamification/settings.py
+++ canvas_gamification/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'accounts.middlewares.login_overlay_middleware'
 ]
 
