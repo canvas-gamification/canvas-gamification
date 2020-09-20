1d50122272ee8e5a0f83c981141fcfbc2c4d9a97
keyvankhademi <keyvankhademi@gmail.com>
2020-09-20 13:16:24 -0700

    add mock course

---
+++ canvas/canvasapi_mock.py
@@ -0,0 +1,43 @@
+class Canvas(object):
+    def __init__(self, base_url, access_token):
+        pass
+
+    def get_course(self, course, use_sis_id=False, **kwargs):
+        return Course()
+
+
+class Course(object):
+    attributes = {
+        'name': 'Mock Course'
+    }
+
+    def create_assignment(self, assignment, **kwargs):
+        return Assignment()
+
+    def create_assignment_group(self, **kwargs):
+        return AssignmentGroup()
+
+    def get_assignment(self, assignment, **kwargs):
+        return Assignment()
+
+    def get_users(self, **kwargs):
+        return [User() for x in range(10)]
+
+    def submissions_bulk_update(self, **kwargs):
+        pass
+
+
+class AssignmentGroup(object):
+    id = 1
+
+
+class Assignment(object):
+    id = 1
+
+    def submissions_bulk_update(self, **kwargs):
+        pass
+
+
+class User(object):
+    id = 1
+    name = "Firstname Lastname"
