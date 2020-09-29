ef677c3eac1302e4b38d13a093da87282566aaf2
Carson Ricca <carsonricca28@gmail.com>
2020-09-28 19:32:47 -0700

    Fixed File Format

--- accounts/static/accounts/js/register.js
+++ accounts/static/accounts/js/register.js
@@ -1,28 +1,23 @@
-$(function() {
-    $('input[open_modal="true"]').on('change', function () {
-        if($(this).is(":checked")) {
-            $("#registration_modal").modal({backdrop: 'static', keyboard: false});
+$(function () {
+    $("input[open_modal=\"true\"]").on("change", function () {
+        if ($(this).is(":checked")) {
+            $("#registration_modal").modal({backdrop: "static", keyboard: false});
         }
     });
 
-    $('#terms_and_conditions_link').bind('click', function(){
-        $("#registration_modal").modal({backdrop: 'static', keyboard: false});
+    $("#terms_and_conditions_link").bind("click", function () {
+        $("#registration_modal").modal({backdrop: "static", keyboard: false});
     });
 
-    $('#cancel_terms_and_conditions').on('click', function(){
-        if($('input[open_modal="true"]').is(":checked")) {
-            $('input[open_modal="true"]').prop('checked', false).trigger('change');
+    $("#cancel_terms_and_conditions").on("click", function () {
+        if ($("input[open_modal=\"true\"]").is(":checked")) {
+            $("input[open_modal=\"true\"]").prop("checked", false).trigger("change");
         }
     });
 
-    $('#consent_terms_and_conditions').on('click', function (){
-       if(!$('input[open_modal="true"]').is(":checked")) {
-           $('input[open_modal="true"]').prop('checked', true).trigger('change');
-       }
+    $("#consent_terms_and_conditions").on("click", function () {
+        if (!$("input[open_modal=\"true\"]").is(":checked")) {
+            $("input[open_modal=\"true\"]").prop("checked", true).trigger("change");
+        }
     });
 });
-
-
-
-
-
