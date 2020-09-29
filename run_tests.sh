a51ab43b0909f6e96cf37e9eef9585e932bd60b4
keyvankhademi <keyvankhademi@gmail.com>
2020-09-29 02:01:50 -0700

    Wait 10 sec for db to initialize

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sleep 10
 python manage.py test
--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 10
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
 python manage.py runserver 0.0.0.0:8000
