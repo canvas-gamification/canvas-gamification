cdc7b1758c5900a059a063965e1ad222e573693a
Carson Ricca <carsonricca28@gmail.com>
2020-10-05 21:08:28 -0700

    Added AJAX Call for Saving Consent Information

--- accounts/static/accounts/js/register.js
+++ accounts/static/accounts/js/register.js
@@ -1,26 +1,49 @@
 $(function () {
-    const open_modal = "input[open_modal=\"true\"]";
+    const checkbox = $("input[open_modal=\"true\"]");
+    const registration_modal = $("#registration_modal");
 
-    $(open_modal).on("change", function () {
+    checkbox.on("change", function () {
         if ($(this).is(":checked")) {
-            $("#registration_modal").modal({backdrop: "static", keyboard: false});
+            registration_modal.modal({backdrop: "static", keyboard: false});
         }
     });
 
     $("#terms_and_conditions_link").bind("click", function () {
-        $("#registration_modal").modal({backdrop: "static", keyboard: false});
+        registration_modal.modal({backdrop: "static", keyboard: false});
     });
 
-    $("#cancel_terms_and_conditions").on("click", function () {
-        if (!$(open_modal).is(":checked")) {
-            $(open_modal).prop("checked", true).trigger("change");
+    $("#close_consent_modal").on("click", function () {
+        if (!checkbox.is(":checked")) {
+            checkbox.prop("checked", true).trigger("change");
         }
     });
 
     $("#consent_terms_and_conditions").on("click", function () {
-        if (!$(open_modal).is(":checked")) {
-            $(open_modal).prop("checked", true).trigger("change");
+        submit();
+        if (!checkbox.is(":checked")) {
+            checkbox.prop("checked", true).trigger("change");
         }
-    $("#informed_consent_form").submit();
     });
 });
+
+function submit() {
+    const registration_modal = $("#registration_modal");
+    const url = $("#informed_consent_form").attr("url");
+    $.ajax({
+        url: url,
+        data: {
+            legal_first_name: $("#legal_first_name").val(),
+            legal_last_name: $("#legal_last_name").val(),
+            student_number: $("#student_number").val(),
+            date: $("#date").val()
+        },
+        dataType: "json",
+        method: "POST",
+        success: function () {
+            registration_modal.modal("hide");
+        },
+        error: function () {
+            alert("Error");
+        }
+    });
+}
