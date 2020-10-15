36e1195ba83cf9a3e0673c350471ab8cac854a6b
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2020-10-14 21:30:51 -0700

    Issue 55 - Registration Flow (#58)
    * Initial Changes
    
    * Removed Consent from Register.html
    
    * Activate test page
    
    * Add Informed Consent to activation_confirm.html
    
    * Fix Flake8 Issue
    
    * Cleaned Up Form
    
    * Added CSRF Token
    
    * Add user to consent form
    
    * Add success url to consent form
    
    * Add Success Url and User ID to Ajax Call
    
    * Changed Header Title
    
    * Add permission to User Consent API
    
    * Fix base.html
    
    * Add delete consent api
    
    * Bug fix
    
    * bug fix
    
    * bug fix 2
    
    * Added Withdraw Functionality
    
    * Fix Flake8 Issues
    
    * Keep consent entities
    
    * Don't Save Student Number
    
    * Changed Failed to Error
    
    Co-authored-by: Keyvan <keyvankhademi@gmail.com>

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, re_path
 
 from accounts.views import signup_view, activate, UserProfileView, PasswordChangeView, password_change_done_view, \
-    forgot_password_view, password_reset_view, LoginView
+    forgot_password_view, password_reset_view, LoginView, consent_view
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
@@ -18,4 +18,6 @@ urlpatterns = [
     path('forgot-password/', forgot_password_view, name="password_forgot"),
     re_path('reset-password/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
             password_reset_view, name='password_reset'),
+
+    path('consent/', consent_view, name='consent')
 ]
