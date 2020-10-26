490ba4d8ecd77c23ede2129fdd6453919f76ad8e
Keyvan <keyvankhademi@gmail.com>
2020-10-25 17:32:28 -0700

    Use junit xml

--- course/grader/grader.py
+++ course/grader/grader.py
@@ -33,22 +33,38 @@ class JunitGrader(Grader):
     }
     BASE_URL = JUDGE0_HOST
 
+    def get_compiler_script(self, submission):
+        f = open('./course/grader/junit_compiler.sh', 'r')
+        compiler_script = f.read()
+        compiler_script = compiler_script.replace("{{user_code_filename}}",
+                                                  submission.question.additional_file_name or "")
+        f.close()
+        return compiler_script
+
     def get_source_code(self, submission):
         code = render_text(submission.question.junit_template, submission.uqj.get_variables())
         return code.replace("{{code}}", submission.answer)
 
     def get_additional_file(self, submission):
+        zipfile = BytesIO()
+        z = ZipFile(zipfile, "w")
+
+        # Junit jar file
+        with open('./course/grader/junit-platform-console-standalone-1.6.2.jar', 'rb') as f:
+            z.writestr('junit-platform-console-standalone-1.6.2.jar', f.read())
+
+        # Junit template file
+        z.writestr("MainTest.java", self.get_source_code(submission))
+
+        # User code
         code = render_text(submission.answer, submission.uqj.get_variables())
         filename = submission.question.additional_file_name
 
-        if not filename:
-            return None
+        if filename:
+            z.writestr(filename, code)
 
-        zipfile = BytesIO()
-        z = ZipFile(zipfile, "w")
-        z.writestr(filename, code)
+        # End of writing to zipfile
         z.close()
-
         return encodebytes(zipfile.getvalue()).decode("UTF-8").strip()
 
     def grade(self, submission):
@@ -57,7 +73,18 @@ class JunitGrader(Grader):
         if submission.in_progress:
             return False, 0
 
-        return (True, 1) if submission.results[0]['status']['id'] == 3 else (False, 0)
+        results = submission.get_decoded_results()
+        total_testcases = len(results)
+        correct_testcases = 0
+
+        for result in results:
+            if result['status'] == 'PASS':
+                correct_testcases += 1
+
+        if total_testcases == 0:
+            return False, 0
+
+        return correct_testcases == total_testcases, correct_testcases / total_testcases
 
     def evaluate(self, submission):
         submission.results = []
@@ -77,12 +104,12 @@ class JunitGrader(Grader):
             data={
                 "base64_encoded": False,
                 "wait": False,
-                "source_code": self.get_source_code(submission),
-                "language_id": 5,
+                "source_code": self.get_compiler_script(submission),
+                "language_id": 46,
                 "additional_files": self.get_additional_file(submission),
-                "compiler_options": submission.question.additional_file_name,
             },
             headers=self.HEADERS,
         )
+        print(r.json())
         submission.tokens.append(r.json()['token'])
         self.evaluate(submission)
