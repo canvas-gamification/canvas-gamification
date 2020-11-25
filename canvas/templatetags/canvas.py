3c6f9cbe5f3e8ff9bfc9d83dbd6ba763e8cef35b
Rachelle Gelden <rachelle@gelden.com>
2020-11-24 22:50:30 -0800

    CG-87: updated text for open event btn

--- canvas/templatetags/canvas.py
+++ canvas/templatetags/canvas.py
@@ -15,4 +15,29 @@ def is_allowed_to_open_course(course, user):
 
 @register.simple_tag
 def is_allowed_to_open_event(event, user):
-    return event.has_view_permission(user)
+    return event.has_view_permission(user) or event.status == "Closed"
+
+
+@register.simple_tag
+def event_button_text(event, user):
+    if event.has_edit_permission(user):
+        return "Edit"
+    if event.is_open() and event.course.is_registered(user):
+        if event.type == "EXAM":
+            return "Take Exam"
+        elif event.type == "ASSIGNMENT":
+            return "Complete Assignment"
+        else:
+            return "Start Practice"
+    elif event.status == "Closed" and event.course.is_registered(user):
+        return "View Results"
+    elif event.status == "Not available yet" and event.course.is_registered(user):
+        return "Not Available"
+    else:
+        return "Open"
+
+
+
+
+
+
