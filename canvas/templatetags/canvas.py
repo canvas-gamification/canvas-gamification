d9b22cc8543364df7eed7e600b7aa82fe278e6dc
Rachelle Gelden <43148498+rachellegelden@users.noreply.github.com>
2020-11-25 18:42:47 -0800

    CG-87: updated text for open event button (#88)
    * CG-87: updated text for open event btn
    
    * CG-87: removed extra whitespace
    
    * CG-87: removed logic from template
    
    * CG-87: removed whitespace
    
    * CG: removed event.status usage

--- canvas/templatetags/canvas.py
+++ canvas/templatetags/canvas.py
@@ -15,4 +15,23 @@ def is_allowed_to_open_course(course, user):
 
 @register.simple_tag
 def is_allowed_to_open_event(event, user):
-    return event.has_view_permission(user)
+    return event.has_view_permission(user) or event.can_view_results(user)
+
+
+@register.simple_tag
+def event_button_text(event, user):
+    if event.has_edit_permission(user):
+        return "Open"
+    if event.is_allowed_to_open(user):
+        if event.type == "EXAM":
+            return "Take Exam"
+        elif event.type == "ASSIGNMENT":
+            return "Complete Assignment"
+        else:
+            return "Start Practice"
+    elif event.can_view_results(user):
+        return "View Results"
+    elif event.cannot_access_event_yet(user):
+        return "Not Available"
+    else:
+        return "Open"
