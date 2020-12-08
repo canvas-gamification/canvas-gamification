b6689ab7d547bad673532e6da6efe1acaa7bb841
Rachelle Gelden <43148498+rachellegelden@users.noreply.github.com>
2020-12-08 15:53:08 -0800

    Added tokens received to submission feedback and past submissions table (#106)
    * added tokens received to submissions
    
    * removed whitespace and updated wording
    
    * fix minor problems
    
    Co-authored-by: keyvankhademi <keyvankhademi@gmail.com>

--- course/models/models.py
+++ course/models/models.py
@@ -325,6 +325,18 @@ class Submission(PolymorphicModel):
 
         return "Wrong"
 
+    @property
+    def tokens_received(self):
+        return self.uqj.tokens_received
+
+    @property
+    def token_value(self):
+        return get_token_value(self.question.category, self.question.difficulty)
+
+    @property
+    def formatted_tokens_received(self):
+        return str(self.tokens_received) + "/" + str(self.token_value)
+
     def calculate_grade(self, commit=True):
         if self.finalized:
             return
@@ -353,7 +365,7 @@ class Submission(PolymorphicModel):
 
         if not self.in_progress and (self.is_correct or self.is_partially_correct or self.question.is_exam):
             user_question_junction = self.uqj
-            received_tokens = self.grade * get_token_value(self.question.category, self.question.difficulty)
+            received_tokens = self.grade * self.token_value
             token_change = received_tokens - user_question_junction.tokens_received
 
             if self.question.is_exam or token_change > 0:
