2f54657b463af4072d9a8e4585bebc46ea70becb
opeyem1a <opeyadeyemi@gmail.com>
2021-02-02 03:13:42 -0800

    implementing course api

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from accounts.models import UserConsent, MyUser
+from canvas.models import CanvasCourse
 from course.models.models import Question, MultipleChoiceQuestion, QuestionCategory
 from general.models import ContactUs
 from utils.recaptcha import validate_recaptcha
@@ -72,3 +73,24 @@ class UserStatsSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyUser
         fields = ['pk', 'successRateByCategory']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    is_registered = serializers.SerializerMethodField('get_is_registered')
+
+    def get_is_registered(self, course):
+        user = None
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            user = request.user
+
+        # if user is not logged in or the request has no user attached
+        if user is None or not user.is_authenticated:
+            return False
+
+        return course.is_registered(user)
+
+    class Meta:
+        model = CanvasCourse
+        fields = ['id', 'mock', 'name', 'url', 'course_id', 'token', 'allow_registration', 'visible_to_students',
+                  'start_date', 'end_date', 'instructor', 'status', 'is_registered']
--- api/views.py
+++ api/views.py
@@ -3,8 +3,9 @@ from rest_framework import viewsets, mixins
 
 from accounts.models import UserConsent, MyUser
 from api.permissions import TeacherAccessPermission, UserConsentPermission
-from api.serializers import QuestionSerializer, MultipleChoiceQuestionSerializer,\
-    UserConsentSerializer, ContactUsSerializer, QuestionCategorySerializer, UserStatsSerializer
+from api.serializers import QuestionSerializer, MultipleChoiceQuestionSerializer, \
+    UserConsentSerializer, ContactUsSerializer, QuestionCategorySerializer, UserStatsSerializer, CourseSerializer
+from canvas.models import CanvasCourse
 from course.models.models import Question, MultipleChoiceQuestion, QuestionCategory
 
 
@@ -37,3 +38,44 @@ class QuestionCategoryViewSet(viewsets.ReadOnlyModelViewSet):
 class UserStatsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = MyUser.objects.all()
     serializer_class = UserStatsSerializer
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = CourseSerializer
+
+    """
+    if the user is not authenticated
+        return all the courses visible to students
+        and the is_registered attribute to false (manually set? in serializer?)
+    if its authenticated and is teacher
+        return all of the courses
+    if a student is authenticated
+        return all the courses with the correct value for is_registered (in serializer)
+    
+    also you should be able to filter the courses by is_registered
+    and you might also need to filter by is_active
+        which means the user should be registered and the course should be "In Session"
+    """
+
+    def get_queryset(self):
+        COURSE_ACTIVE = 'In Session'
+        user = self.request.user
+        # true = active courses, false = all courses
+        active = self.request.query_params.get('active', False)
+        # true = filter by if user is registered, false = no filtering on registration status
+        registered = self.request.query_params.get('registered', False)
+
+        if not user.is_authenticated:
+            return CanvasCourse.objects.filter(visible_to_students=True).all()
+        else:
+            queryset = CanvasCourse.objects
+            # if request comes from a teacher, return all the courses they instruct
+            if user.is_teacher:
+                queryset = queryset.filter(instructor=user)
+            elif user.is_student:
+                if active:
+                    queryset = queryset.filter(status=COURSE_ACTIVE)
+                if registered or active:
+                    queryset = queryset.filter(is_registered=True)
+
+            return queryset.all()
