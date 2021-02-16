bb883079525e21ce2ca4b2e32517856fe6846411
opeyem1a <opeyadeyemi@gmail.com>
2021-02-16 13:45:56 -0800

    fixing course api bug with boolean query parameters

--- api/views.py
+++ api/views.py
@@ -93,14 +93,16 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
-        registered = self.request.query_params.get('registered', False)
+        registered = self.request.query_params.get('registered', None)
+        if registered:
+            registered = registered.lower() == 'true'
 
         queryset = CanvasCourse.objects.all()
 
         if not user.is_authenticated or user.is_student:
             queryset = queryset.filter(visible_to_students=True).all()
 
-        if registered:
+        if registered is True:
             registered_ids = user.canvascourseregistration_set.filter(is_verified=True, is_blocked=False)\
                 .values_list('course_id', flat=True)
             queryset = queryset.filter(pk__in=registered_ids)
