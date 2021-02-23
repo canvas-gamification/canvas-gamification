6bd9275166850e97aa8f497ecfb1588810782844
Carson Ricca <carsonricca28@gmail.com>
2021-02-22 21:34:15 -0800

    Finish Account Migration

--- api/views/register.py
+++ api/views/register.py
@@ -1,6 +1,8 @@
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, status
+from rest_framework.response import Response
 
 from accounts.models import MyUser
+from accounts.utils.email_functions import send_activation_email
 from api.serializers.register import UserRegistrationSerializer
 
 
@@ -9,6 +11,10 @@ class UserRegistrationViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = MyUser.objects.all()
 
     def create(self, request, *args, **kwargs):
-        user = super().create(request, *args, **kwargs)
-        # send_activation_email(request, user)
-        return user
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        print(user)
+        headers = self.get_success_headers(serializer.data)
+        send_activation_email(request, user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
