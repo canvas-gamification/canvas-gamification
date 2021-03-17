5a059a3746677b20d785db7fd21562b7be093c3a
opeyem1a <opeyadeyemi@gmail.com>
2021-03-17 14:32:07 -0700

    all serializers imported in __init__.py

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -2,12 +2,15 @@ from .action import ActionsSerializer
 from .canvas_course_registration import CanvasCourseRegistrationSerializer
 from .contact_us import ContactUsSerializer
 from .course import CourseSerializer
+from .event import EventSerializer
 from .faq import FAQSerializer
 from .java_question import JavaQuestionSerializer
 from .multiple_choice_question import MultipleChoiceQuestionSerializer
 from .parsons_question import ParsonsQuestionSerializer
 from .question import QuestionSerializer
 from .question_category import QuestionCategorySerializer
+from .token_use import TokenUseSerializer
+from .token_use_option import TokenUseOptionSerializer
 from .token_value import TokenValueSerializer
 from .uqj import UQJSerializer
 from .user_consent import UserConsentSerializer
