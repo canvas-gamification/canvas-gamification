5535ad1bc39fa0c6a8595b2fe43172962cf0b003
opeyem1a <opeyadeyemi@gmail.com>
2021-03-23 06:56:19 -0700

    fixing views/action

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -4,18 +4,23 @@ NOTE: if serializer A imports serializer B, then A **must** be imported in this 
 """
 
 # Serializers that don't import any other serializers
+from .utils import UpdateListSerializer
+
 from .action import ActionsSerializer
 from .contact_us import ContactUsSerializer
 from .faq import FAQSerializer
-from .java_question import JavaQuestionSerializer
-from .multiple_choice_question import MultipleChoiceQuestionSerializer
-from .parsons_question import ParsonsQuestionSerializer
+from .java_question import JavaQuestionSerializer, JavaSubmissionSerializer
+from .multiple_choice_question import MultipleChoiceQuestionSerializer, MultipleChoiceSubmissionSerializer
+from .parsons_question import ParsonsQuestionSerializer, ParsonsSubmissionSerializer
 from .question_category import QuestionCategorySerializer
 from .token_value import TokenValueSerializer
 from .user_consent import UserConsentSerializer
 from .user_stats import UserStatsSerializer
 from .token_use_option import TokenUseOptionSerializer
 from .event import EventSerializer
+from .reset_password import ResetPasswordSerializer
+from .register import UserRegistrationSerializer
+from .update_profile import UpdateProfileSerializer
 
 # Serializers that depend on other serializers
 from .token_use import TokenUseSerializer
@@ -23,3 +28,4 @@ from .question import QuestionSerializer
 from .uqj import UQJSerializer
 from .canvas_course_registration import CanvasCourseRegistrationSerializer
 from .course import CourseSerializer
+
