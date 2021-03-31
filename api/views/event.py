0539888bf3b2122f2db7af0567a206f7183cf1a6
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-03-30 21:31:41 -0700

    Revert "Added API functionality for returning event types"
    This reverts commit 27b0c031

--- api/views/event.py
+++ api/views/event.py
@@ -1,12 +1,9 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, mixins
-from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 
 from api.serializers import EventSerializer
 from canvas.models import Event
-from canvas.models import EVENT_TYPE_CHOICES
 
 
 class EventViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.DestroyModelMixin, mixins.UpdateModelMixin,
@@ -19,13 +16,6 @@ class EventViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.Destro
     permission_classes = [IsAuthenticated, ]
     filter_backends = [DjangoFilterBackend, ]
     filterset_fields = ['course', ]
-
-    @action(detail=False, methods=['get'], url_path="get-event-types")
-    def get_event_types(self, request):
-        event_types = [value for (key,value) in EVENT_TYPE_CHOICES]
-        return Response({
-            "event_types": event_types,
-        })
 
     def get_queryset(self):
         return Event.objects.all()
