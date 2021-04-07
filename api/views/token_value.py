4a2937fbc20e6c1ad093ec8dba61830f337557c4
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-04-06 22:05:18 -0700

    Fixed flake8 issues

--- api/views/token_value.py
+++ api/views/token_value.py
@@ -1,6 +1,5 @@
 from rest_framework import viewsets, mixins
 from rest_framework.decorators import action
-from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
 from api.permissions import TeacherAccessPermission
