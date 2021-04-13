9098b10e3faee754eb8b787294afb76eba556be6
opeyem1a <opeyadeyemi@gmail.com>
2021-04-13 00:03:48 -0700

    fix flake8 issues

--- api/views/course.py
+++ api/views/course.py
@@ -1,7 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, viewsets
 from rest_framework.decorators import action
-from rest_framework.exceptions import ValidationError, bad_request
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
