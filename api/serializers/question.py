0cf473562c77daefd9c1cc788c089e5a2fa8ced3
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-04-12 22:07:47 -0700

    Added field in question serializer to get UQJ status

--- api/serializers/question.py
+++ api/serializers/question.py
@@ -6,9 +6,14 @@ from course.models.models import Question
 
 class QuestionSerializer(serializers.ModelSerializer):
     event = EventSerializer()
+    status = serializers.SerializerMethodField('get_uqj_status')
 
     class Meta:
         model = Question
         fields = ['id', 'title', 'max_submission_allowed', 'time_created', 'time_modified', 'author', 'category',
                   'difficulty', 'is_verified', 'token_value', 'success_rate', 'type_name', 'event', 'is_sample',
-                  'category_name', 'parent_category_name', 'course_name', 'event_name', 'author_name', ]
+                  'category_name', 'parent_category_name', 'course_name', 'event_name', 'author_name', 'status', ]
+
+    def get_uqj_status(self, obj):
+        request = self.context.get('request', None)
+        return request.user.question_junctions.filter(question__pk=obj.id).first().status
