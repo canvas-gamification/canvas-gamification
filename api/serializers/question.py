606ab14dfd2942a026f8ce67e7d3a428508197a8
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-04-13 14:15:03 -0700

    UQJ Highlighting Api Set up (#159)
    * Added field in question serializer to get UQJ status
    
    * Fixed bug when request would be None type
    
    * Minor improvements
    
    Co-authored-by: keyvankhademi <keyvankhademi@gmail.com>

--- api/serializers/question.py
+++ api/serializers/question.py
@@ -6,10 +6,17 @@ from course.models.models import Question
 
 class QuestionSerializer(serializers.ModelSerializer):
     event = EventSerializer()
+    status = serializers.SerializerMethodField('get_uqj_status')
 
     class Meta:
         model = Question
         fields = ['id', 'title', 'max_submission_allowed', 'time_created', 'time_modified', 'author', 'category',
                   'difficulty', 'is_verified', 'token_value', 'success_rate', 'type_name', 'event', 'is_sample',
                   'category_name', 'parent_category_name', 'course_name', 'event_name', 'author_name', 'is_open',
-                  'is_exam', 'is_exam_and_open']
+                  'is_exam', 'is_exam_and_open', 'status']
+
+    def get_uqj_status(self, obj):
+        request = self.context.get('request', None)
+        if request is None:
+            return ''
+        return request.user.question_junctions.get(question__pk=obj.id).status
