ba2204ba10f573463d4168b7f4b6017e8ad9a680
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-05-07 12:21:33 -0700

    Added action to return event types from backend

--- api/views/event.py
+++ api/views/event.py
@@ -1,9 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from api.serializers import EventSerializer
-from canvas.models import Event
+from canvas.models import Event, EVENT_TYPE_CHOICES
 
 
 class EventViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin, mixins.UpdateModelMixin,
@@ -19,3 +21,10 @@ class EventViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin, mixins.Upd
 
     def get_queryset(self):
         return Event.objects.all()
+
+    @action(detail=False, methods=['get'], url_path="get-event-types")
+    def get_event_types(self, request, pk=None):
+        """
+        Returns a dictionary of the defined event types
+        """
+        return Response(dict(EVENT_TYPE_CHOICES))
