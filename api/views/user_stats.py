21264455006f3becd82bc5959dfd2a1338ed5d64
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-05-11 09:59:17 -0400

    Update User Stats Permissions (#167)
    - Users can only access their user-stats.

--- api/views/user_stats.py
+++ api/views/user_stats.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from accounts.models import MyUser
 from api.serializers import UserStatsSerializer
 
 
 class UserStatsViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = MyUser.objects.all()
+    def get_queryset(self):
+        return MyUser.objects.filter(id=self.request.user.id)
     serializer_class = UserStatsSerializer
+    permission_classes = [IsAuthenticated, ]
