f9917962e5b71482d0e5a287b9a27163d215c590
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-05-11 21:41:51 -0400

    Add Permission Class

--- api/views/uqj.py
+++ api/views/uqj.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
 
 from api.pagination import BasePagination
+from api.permissions import UQJViewPermission
 from api.serializers import UQJSerializer
 
 
@@ -12,7 +13,7 @@ class UQJViewSet(viewsets.ReadOnlyModelViewSet):
     + Standard ordering is applied on the field 'last_viewed'
     """
     serializer_class = UQJSerializer
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticated, UQJViewPermission, ]
     pagination_class = BasePagination
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, ]
     ordering_fields = ['last_viewed', ]
@@ -20,13 +21,13 @@ class UQJViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
-        uqj = user.question_junctions.filter(user=user)
-        accessible_uqj = []
-        for temp in uqj:
-            if temp.question.has_view_permission(user):
-                accessible_uqj.append(temp)
-            # TODO remove uqj from queryset if user does not have view permission.
-            # else:
-            # uqj = uqj.exclude(temp)
-        print(accessible_uqj)
+        uqj = user.question_junctions.all()
+        # accessible_uqj = []
+        # for temp in uqj:
+        #     if temp.question.has_view_permission(user):
+        #         accessible_uqj.append(temp)
+        #     # TODO remove uqj from queryset if user does not have view permission.
+        #     # else:
+        #     # uqj = uqj.exclude(temp)
+        # print(accessible_uqj)
         return uqj
