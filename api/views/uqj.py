ae55c56542eae1e9b5b1d138fdefdce47d82059b
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-05-11 21:18:53 -0400

    Initial Attempts at Fixing UQJ Permissions

--- api/views/uqj.py
+++ api/views/uqj.py
@@ -20,4 +20,13 @@ class UQJViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
-        return user.question_junctions.all()
+        uqj = user.question_junctions.filter(user=user)
+        accessible_uqj = []
+        for temp in uqj:
+            if temp.question.has_view_permission(user):
+                accessible_uqj.append(temp)
+            # TODO remove uqj from queryset if user does not have view permission.
+            # else:
+            # uqj = uqj.exclude(temp)
+        print(accessible_uqj)
+        return uqj
