8c7a876d5e6dd33f2124e991f204f0873afd4e12
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-05-20 17:26:47 -0400

    Only Author Can Delete A Question
    - Tested functionality and it works. Only the author of the question is allowed to delete it.

--- api/permissions.py
+++ api/permissions.py
@@ -17,3 +17,10 @@ class UserConsentPermission(permissions.IsAuthenticated):
 
     def has_object_permission(self, request, view, obj):
         return request.user == obj.user
+
+
+class IsOwnerOrReadOnly(permissions.IsAuthenticated):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
