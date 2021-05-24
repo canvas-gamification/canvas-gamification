d39fd939f089206f51e9fb57b4358d79da451118
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-05-24 14:35:39 -0700

    Fixed serializer so it will work with anonymous users, and added extra validation

--- api/serializers/reset_password.py
+++ api/serializers/reset_password.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.password_validation import validate_password
+from django.utils.encoding import force_text
+from django.utils.http import urlsafe_base64_decode
 
 from rest_framework import serializers
 
@@ -6,27 +8,29 @@ from accounts.models import MyUser
 
 
 class ResetPasswordSerializer(serializers.ModelSerializer):
+    uid = serializers.CharField(write_only=True, required=True)
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
     old_password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = MyUser
-        fields = ['old_password', 'password', 'password2']
+        fields = ['uid', 'old_password', 'password', 'password2']
 
     def validate(self, attrs):
+        uid = force_text(urlsafe_base64_decode(attrs['uid']))
+        user = MyUser.objects.get(pk=uid)
+        if not user.check_password(attrs['old_password']):
+            raise serializers.ValidationError({"old_password": "Old password is not correct"})
+        if attrs['old_password'] == attrs['password']:
+            raise serializers.ValidationError({"password": "New Password is the same as the old password"})
         if attrs['password'] != attrs['password2']:
             raise serializers.ValidationError({"password": "Password fields didn't match."})
         return attrs
 
-    def validate_old_password(self, value):
-        user = self.context['request'].user
-        if not user.check_password(value):
-            raise serializers.ValidationError({"old_password": "Old password is not correct"})
-        return value
-
     def create(self, validated_data):
-        user = self.context['request'].user
+        uid = force_text(urlsafe_base64_decode(validated_data['uid']))
+        user = MyUser.objects.get(pk=uid)
         user.set_password(validated_data['password'])
         user.save()
         return user
