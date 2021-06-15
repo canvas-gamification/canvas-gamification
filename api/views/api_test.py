0b71bc2bff236286638df7bf15d08092a7614597
Jarod <jarodalgra@gmail.com>
2021-06-14 20:30:16 -0700

    add test data

--- api/views/api_test.py
+++ api/views/api_test.py
@@ -2,9 +2,14 @@ from rest_framework import viewsets
 
 
 from course.models.models import TestModel
+from accounts.models import MyUser
+from course.models.models import TestModel
 from api.serializers import TestSerializer
 
 class ApiTestViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
+        # return MyUser.objects.all() #Use with actual users in database
         return TestModel.objects.all()
+        
+        
     serializer_class = TestSerializer
