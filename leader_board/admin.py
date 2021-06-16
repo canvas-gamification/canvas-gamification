bbeb7b5840372d773113ab621f3d9276d0b3ab3d
nakheem <rayman55@live.ca>
2021-06-15 19:18:38 -0700

    Changing admin registar

--- leader_board/admin.py
+++ leader_board/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import Leader_Board, LeaderBoardAssignedStudents
 
 admin.site.register(Leader_Board)
-admin.site.register(LeaderBoardAssignedStudents)
+admin.site.register(LeaderBoardAssignedStudents)                       
