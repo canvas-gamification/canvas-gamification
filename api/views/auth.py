b4b2e02f26881bd93a29f2fd06ce852f4e2b903d
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-06-18 14:47:53 -0700

    Added id to api-token-auth to fix profile details

--- api/views/auth.py
+++ api/views/auth.py
@@ -12,6 +12,7 @@ class ObtainAuthTokenView(ObtainAuthToken):
         token, created = Token.objects.get_or_create(user=user)
         return Response({
             'token': token.key,
+            'id': user.id,
             'first_name': user.first_name,
             'last_name': user.last_name,
             'username': user.username,
