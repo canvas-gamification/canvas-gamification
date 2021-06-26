53ed8ddbc4efeb955b99406f9b770c6823e1033e
JarodAlgra <jarodalgra@gmail.com>
2021-06-25 19:09:22 -0700

    Create test_serializer.py
    add Test Serializer

---
+++ api/serializers/test_serializer.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from api.serializers import EventSerializer
+from accounts.models import MyUser
+from course.models.models import TestModel
+
+class TestSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        ## Use with MyUser model
+        # model = MyUser
+        # fields = ['first_name', 'tokens']
+
+        ## Use with TestModel
+        model = TestModel
+        fields = ['username', 'tokens']
+
