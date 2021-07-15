37a40390be9c83beeea59baf458f4e2b234ce522
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-07-15 15:50:07 -0400

    Update Event API Permissions

--- api/views/event.py
+++ api/views/event.py
@@ -1,21 +1,21 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from api.permissions import IsOwnerOrReadOnly
 from api.serializers import EventSerializer
 from canvas.models import Event, EVENT_TYPE_CHOICES
 
 
-class EventViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin, mixins.UpdateModelMixin,
-                   mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+class EventViewSet(viewsets.ModelViewSet):
     """
     Optional Parameters
     - Base filtering on the 'course' parameter
     """
     serializer_class = EventSerializer
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly, ]
     filter_backends = [DjangoFilterBackend, ]
     filterset_fields = ['course', ]
 
@@ -28,13 +28,6 @@ class EventViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin, mixins.Upd
         Returns a dictionary of the defined event types
         """
         return Response(EVENT_TYPE_CHOICES)
-
-    @action(detail=False, methods=['get'], url_path="get-all-events")
-    def get_all_events(self, request):
-        """
-        Returns a dictionary of all events.
-        """
-        return Response(self.get_queryset().values())
 
     @action(detail=False, methods=['post'], url_path="duplicate-event")
     def duplicate_event(self, request):
