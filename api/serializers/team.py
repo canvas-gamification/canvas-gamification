180a6374d60d43f2a8084ebd6c591cf98889714e
JarodAlgra <jarodalgra@gmail.com>
2021-07-19 12:56:15 -0700

    Reformatted with PyCharm

--- api/serializers/team.py
+++ api/serializers/team.py
@@ -1,4 +1,3 @@
-
 from rest_framework import serializers
 
 from accounts.models import MyAnonymousUser
@@ -10,15 +9,12 @@ from canvas.utils.utils import get_course_registration
 
 class TeamSerializer(serializers.ModelSerializer):
 
-
     def get_user(self):
         user = MyAnonymousUser()
         request = self.context.get("request")
         if request and hasattr(request, "user"):
             user = request.user
         return user
-
-
 
     class Meta:
         model = Team
