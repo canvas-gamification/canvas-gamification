5d0840ce566a3bfa52972104a5e09ff9c52b18d4
Aashish Raizada <60690063+Aashishraizada@users.noreply.github.com>
2021-07-23 18:26:31 -0700

    added endpoints for handling Create/Join/Leave Team functionality

--- api/views/team.py
+++ api/views/team.py
@@ -1,26 +1,50 @@
+from api import serializers
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, viewsets
 from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+
 
 from api.serializers.team import TeamSerializer
-from canvas.models import Team, TeamRegistration, CanvasCourse
+from canvas.models import Team, TeamRegistration, CanvasCourse, MyUser
 from canvas.utils.utils import get_course_registration, get_team_registration
-
 
 
 class TeamViewSet(viewsets.ReadOnlyModelViewSet):
 
     serializer_class = TeamSerializer
 
-    def get_queryset(self):
-        user = self.request.user
-
+    def get_queryset(self, *args):
+        courseId = self.request.query_params.get('courseId', None)
+        course = get_object_or_404(CanvasCourse, pk=courseId)
         queryset = Team.objects.all()
-
+        queryset = queryset.filter(course=course)
         return queryset.all()
+
+
+    @action(detail=True, methods=['get'])
+    def get_team_registration(self, request, pk=None):
+        
+        course = get_object_or_404(CanvasCourse, pk=pk)
+        userId = self.request.query_params.get('userId', None)
+        user = get_object_or_404(MyUser, pk=userId)
+        try:
+            team_reg = TeamRegistration.objects.get(user=user, team__course=course)
+        except ObjectDoesNotExist:
+            return Response({
+                'userId': userId,
+                'teamId': -1
+            })
+       
+        return Response({
+            'userId': team_reg.user.id,
+            'teamId': team_reg.team.id
+        })
+
 
     @action(detail=True, methods=['post'])
     def create_team(self, request, pk=None):
@@ -33,7 +57,15 @@ class TeamViewSet(viewsets.ReadOnlyModelViewSet):
 
         course = get_object_or_404(CanvasCourse, pk=pk)
 
+        # all teams registrations for the user in the requested course
+        all_team_reg = TeamRegistration.objects.filter(user=request.user, team__course=course)
+        
+        # delete other registrations if any exist
+        if(all_team_reg is not None):
+                for reg in all_team_reg:
+                    reg.delete()
 
+                    
         # create a new team with the specified name
         new_team = Team(name=team_name, course=course)
         new_team.save()
@@ -42,6 +74,7 @@ class TeamViewSet(viewsets.ReadOnlyModelViewSet):
         new_team_reg = TeamRegistration(user=request.user, team=new_team)
         new_team_reg.save()
         
+
         return Response({
                 "status": "Created",
             })
@@ -54,7 +87,19 @@ class TeamViewSet(viewsets.ReadOnlyModelViewSet):
         team = get_object_or_404(Team, pk=pk)
         team_reg = get_team_registration(user=request.user, team=team)
 
+        course = team.course
+        
+        # all teams registrations for the user in the requested course
+        all_team_reg = TeamRegistration.objects.filter(user=request.user, team__course=course)
+        
+
+        # if the user is not registered for the selected team
         if team_reg is None:
+            # delete other registrations if any exist already
+            if(all_team_reg is not None):
+                for reg in all_team_reg:
+                    reg.delete()
+
             # create a TeamRegistration for this (user, team) pair if one does not already exist
             team_reg = TeamRegistration(user=request.user, team=team)
             team_reg.save()
@@ -63,3 +108,21 @@ class TeamViewSet(viewsets.ReadOnlyModelViewSet):
                 "status": "Registered",
             })
         
+
+    @action(detail=True, methods=['post'])
+    def leave_team(self, request, pk=None):
+        """
+        This endpoint completes leaving a team for this (user, team) pair
+        """
+        team = get_object_or_404(Team, pk=pk)
+        team_reg = get_team_registration(user=request.user, team=team)
+
+        if team_reg is None:
+            pass
+        
+        else:
+            team_reg.delete()
+
+        return Response({
+                "status": "De-registered",
+            })
