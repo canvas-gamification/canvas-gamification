4a411ef27cc777aee4a7b330c38654969a349f17
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-09-21 09:16:18 -0700

    Fix Flake8

--- general/services/action.py
+++ general/services/action.py
@@ -1,6 +1,3 @@
-from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
-
 from general.models.action import Action, ActionStatus, ActionVerb, ActionObjectType
 
 
