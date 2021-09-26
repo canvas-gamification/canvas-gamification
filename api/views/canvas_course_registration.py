f363ca0ee039741ef5d05e928777b687db804780
Khai Hung <hungluongkhai@gmail.com>
2021-09-26 14:39:13 -0700

    Add is_favourite Add features in course/admin.py.

--- api/views/canvas_course_registration.py
+++ api/views/canvas_course_registration.py
@@ -1,7 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-
 from api.serializers import CanvasCourseRegistrationSerializer
 from canvas.models import CanvasCourseRegistration
 
