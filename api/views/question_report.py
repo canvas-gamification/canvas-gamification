49aa4e42eabd84a9da402b1abe8f47abca05ffdf
kaminskg <gkamin6@gmail.com>
2021-10-22 21:38:36 -0700

    Update the backend so it fetches data from frontend

--- api/views/question_report.py
+++ api/views/question_report.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+
+from accounts.models import MyUser
+from course.models.models import Question
 from general.models.question_report import QuestionReport
 from rest_framework.permissions import IsAuthenticated
 from api.serializers import QuestionReportSerializer
@@ -17,7 +20,12 @@ class QuestionReportViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['post'], url_path='add-report')
     def create_report(self, request, pk=None):
         report = request.data.get('report')
-        QuestionReport.report_details = report
-        QuestionReport.save()
+        report_details = request.data.get('report_details')
+        user_instance = request.data.get('user')
+        user = MyUser.objects.all().filter(username=user_instance.get('username')).get()
+        question_instance = request.data.get('question')
+        question = Question.objects.all().filter(id=question_instance.get('id')).get()
+        question_report = QuestionReport(report=report, report_details=report_details, user=user, question=question)
+        question_report.save()
         return Response(request.data)
 
