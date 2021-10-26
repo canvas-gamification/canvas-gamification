6c1fce3b21211b1e51d0b2628c738accac83cea1
kaminskg <gkamin6@gmail.com>
2021-10-26 16:43:47 -0700

    Add serializer to question report

---
+++ api/serializers/question_report.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from general.models.question_report import QuestionReport
+
+
+class QuestionReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuestionReport
+        fields = ['id', 'user', 'question', 'created_at', 'updated_at', 'typo_in_question', 'typo_in_answer',
+                  'correct_solution_marked_wrong', 'incorrect_solution_marked_right', 'other',
+                  'report_details']
