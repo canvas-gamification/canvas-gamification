c9a328e6807d8707fc93da300e748c3b420b90f6
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-10-25 21:33:13 -0700

    Remove redundant fields from Question serializer

--- api/serializers/question.py
+++ api/serializers/question.py
@@ -16,10 +16,9 @@ class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = ['id', 'title', 'text', 'max_submission_allowed', 'time_created', 'time_modified', 'author',
-                  'category', 'difficulty', 'is_verified', 'token_value', 'success_rate', 'type_name', 'event',
-                  'event_id', 'is_sample', 'category', 'category_id', 'parent_category_name', 'course_name',
-                  'event_name', 'author_name', 'is_open', 'is_exam', 'is_exam_and_open', 'status', 'full_category_name',
-                  'is_author', 'is_practice']
+                  'author_name', 'difficulty', 'is_verified', 'token_value', 'success_rate', 'type_name', 'event',
+                  'event_id', 'category', 'category_id', 'parent_category_name', 'is_sample',
+                  'is_open', 'is_exam', 'is_exam_and_open', 'status', 'is_author', 'is_practice']
 
     def get_uqj_status(self, obj):
         request = self.context.get('request', None)
