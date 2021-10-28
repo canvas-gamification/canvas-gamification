2e6d333dedb11a1c337d3ab6157a7b819c70d2aa
kaminskg <gkamin6@gmail.com>
2021-10-28 11:02:14 -0700

    Update question report serializer. Previous one was for the old model

--- api/serializers/question_report.py
+++ api/serializers/question_report.py
@@ -5,6 +5,4 @@ from general.models.question_report import QuestionReport
 class QuestionReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = QuestionReport
-        fields = ['id', 'user', 'question', 'created_at', 'updated_at', 'typo_in_question', 'typo_in_answer',
-                  'correct_solution_marked_wrong', 'incorrect_solution_marked_right', 'other',
-                  'report_details']
+        fields = ['id', 'user', 'question', 'created_at', 'updated_at', 'report', 'report_details']
