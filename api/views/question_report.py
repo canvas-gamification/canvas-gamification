063e5e7ea374675912a55a0feb52ef594a18c0c7
kaminskg <gkamin6@gmail.com>
2021-11-02 22:09:37 -0700

    Add the queryset back.

--- api/views/question_report.py
+++ api/views/question_report.py
@@ -2,10 +2,12 @@ from rest_framework import viewsets
 
 from rest_framework.permissions import IsAuthenticated
 from api.serializers import QuestionReportSerializer
+from general.models.question_report import QuestionReport
 from general.services.action import create_question_report_action
 
 
 class QuestionReportViewSet(viewsets.ModelViewSet):
+    queryset = QuestionReport.objects.all()
     serializer_class = QuestionReportSerializer
     permission_classes = [IsAuthenticated, ]
 
