e5a1dd22cc7b05c752740ae0b480a523c512406d
Prostigus <prajeetdidden@gmail.com>
2021-11-08 19:36:04 -0800

    Add response when creating report

--- api/views/question_report.py
+++ api/views/question_report.py
@@ -1,6 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from api.serializers import QuestionReportSerializer
 from general.models.question_report import QuestionReport
 from general.services.action import create_question_report_action
@@ -14,3 +16,4 @@ class QuestionReportViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
         create_question_report_action(serializer.data, self.request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
