037c871db6a1b2f8ff70cef4b46bd80d51341749
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-11-16 15:14:33 -0800

    Registration Type (#265)
    * Add Registration Type
    
    * Use Registration Types for Permissions
    
    * Update course has create permission
    
    * Fix PR Comment

--- api/permissions.py
+++ api/permissions.py
@@ -22,6 +22,36 @@ class StudentsMustBeRegisteredPermission(permissions.IsAuthenticated):
         return True
 
 
+class CourseEditPermission(permissions.IsAuthenticated):
+    def has_object_permission(self, request, view, obj):
+        if request.method == "PUT":
+            return obj.has_edit_permission(obj, request.user)
+        return True
+
+
+class CourseCreatePermission(permissions.IsAuthenticated):
+    def has_object_permission(self, request, view, obj):
+        if request.method == "POST":
+            return request.user.is_teacher
+        return True
+
+
+class EventCreatePermission(permissions.IsAuthenticated):
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        if request.method == "POST":
+            return obj.has_create_permission(user)
+        return True
+
+
+class EventEditPermission(permissions.IsAuthenticated):
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        if request.method == "PUT":
+            return obj.has_edit_permission(user)
+        return True
+
+
 class HasDeletePermission(permissions.IsAuthenticated):
     def has_object_permission(self, request, view, obj):
         if request.method == 'DELETE':
