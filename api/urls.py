0b4d1b52e65efa76f14bf67b14a906cf3bffc222
Guy Kaminsky <59747630+kaminskg@users.noreply.github.com>
2021-12-02 12:53:30 -0800

    Question report viewset operations (#260)
    * Add is_favourite Add features in course/admin.py.
    
    * Revert "Add tests for actions"
    
    This reverts commit 0c266f7ef11f38f1fecda43e267f9aa0979ecf7f.
    
    # Conflicts:
    #	general/tests.py
    
    * Revert "Add is_favourite Add features in course/admin.py."
    
    This reverts commit f363ca0ee039741ef5d05e928777b687db804780.
    
    * Revert "Revert "Add tests for actions""
    
    This reverts commit e0f2efee9120b47898d043440725098bb9889cc6.
    
    * Add the viewset for question_report
    
    * Add the ability to create, delete and retrieve a report so the frontend can call these functions
    
    * Revert get_report to work with query and cleanup code
    
    * Remove request parameter in get_report and delete_report
    
    * Update the backend so we retrive a report using a uqj
    
    * Add the queryset back.
    
    * Add response when creating report
    
    * Add user authentication
    
    Co-authored-by: Khai Hung <hungluongkhai@gmail.com>
    Co-authored-by: Anshul <64695943+BeefyAdmiral@users.noreply.github.com>
    Co-authored-by: Prostigus <prajeetdidden@gmail.com>
    Co-authored-by: Keyvan <keyvan@tribe.so>

--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,7 @@ from api.views import QuestionViewSet, SampleMultipleChoiceQuestionViewSet, User
     CourseViewSet, CanvasCourseRegistrationViewSet, ResetPasswordViewSet, UserRegistrationViewSet, \
     UpdateProfileViewSet, SubmissionViewSet, ObtainAuthTokenView, MultipleChoiceQuestionViewSet, JavaQuestionViewSet, \
     ParsonsQuestionViewSet, EventViewSet, TokenUseViewSet, SchemaViewSet, DifficultyViewSet, ChangePasswordViewSet, \
-    UpdateUQJViewSet
+    UpdateUQJViewSet, QuestionReportViewSet
 from api.views.admin import AdminViewSet
 from api.views.course_admin import CourseAdminViewSet
 
@@ -41,6 +41,7 @@ router.register(r'difficulty', DifficultyViewSet, basename='difficulty')
 router.register(r'admin', AdminViewSet, basename='admin')
 router.register(r'course-admin', CourseAdminViewSet, basename='admin-course')
 router.register(r'uqj-update', UpdateUQJViewSet, basename='uqj-update')
+router.register(r'question-report', QuestionReportViewSet, basename='question-report')
 
 app_name = 'api'
 urlpatterns = [
