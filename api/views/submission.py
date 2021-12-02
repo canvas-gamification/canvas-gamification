cbe4795bdb84b0d76ce413408275079288672664
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-12-02 11:10:03 -0800

    Fix Flake8 issues in project

--- api/views/submission.py
+++ api/views/submission.py
@@ -1,7 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters, status
 from rest_framework.decorators import action
-from rest_framework.exceptions import PermissionDenied, ValidationError
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
