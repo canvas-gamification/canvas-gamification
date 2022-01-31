d3fd945bdf3cece6d4aad63b742133590e0e6728
sirusw <50982809+ourinsou@users.noreply.github.com>
2022-01-30 20:07:00 -0800

    Update analytics.py

--- api/views/analytics.py
+++ api/views/analytics.py
@@ -3,10 +3,15 @@ from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
+from analytics.models.java import JavaQuestionAnalytics
+from analytics.models.mcq import MCQQuestionAnalytics
 from analytics.models.models import SubmissionAnalytics, QuestionAnalytics
+from analytics.models.parsons import ParsonsQuestionAnalytics
 from analytics.services.question_analytics import get_all_question_analytics, get_question_analytics
 from analytics.services.submission_analytics import get_submission_analytics, get_all_submission_analytics
 from api.permissions import TeacherAccessPermission
+from api.serializers.question_analytics import MCQQuestionAnalyticsSerializer, JavaQuestionAnalyticsSerializer, \
+    ParsonsQuestionAnalyticsSerializer
 from course.models.models import Submission, Question
 
 
@@ -28,8 +33,21 @@ class QuestionAnalyticsViewSet(viewsets.GenericViewSet):
     permission_classes = [TeacherAccessPermission]
     queryset = QuestionAnalytics.objects.all()
 
+    def get_serialized_data(self, question_analytics):
+        if isinstance(question_analytics, MCQQuestionAnalytics):
+            return MCQQuestionAnalyticsSerializer(question_analytics).data
+        if isinstance(question_analytics, JavaQuestionAnalytics):
+            return JavaQuestionAnalyticsSerializer(question_analytics).data
+        if isinstance(question_analytics, ParsonsQuestionAnalytics):
+            return ParsonsQuestionAnalyticsSerializer(question_analytics).data
+
     def list(self, request):
-        return Response(get_all_question_analytics())
+        get_all_question_analytics()
+        query_set = QuestionAnalytics.objects.all()
+        results = [
+            self.get_serialized_data(analytics) for analytics in query_set
+        ]
+        return Response(results)
 
     @action(detail=False, methods=['get'], url_path='question')
     def question(self, request):
