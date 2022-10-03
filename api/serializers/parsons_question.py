31d6594e400b8a0ca3d6c58e6689d31491788922
yuqi88 <snow34545@gmail.com>
2022-10-03 15:51:15 -0700

    run flake8

--- api/serializers/parsons_question.py
+++ api/serializers/parsons_question.py
@@ -36,7 +36,8 @@ class ParsonsSubmissionSerializer(serializers.ModelSerializer):
         fields = ['pk', 'submission_time', 'answer', 'grade', 'is_correct', 'is_partially_correct', 'finalized',
                   'status', 'tokens_received', 'token_value', 'question', 'get_decoded_stderr', 'get_decoded_results',
                   'get_formatted_test_results', 'get_passed_test_results', 'get_failed_test_results', 'get_num_tests',
-                  'formatted_tokens_received', 'answer_files', 'show_answer', 'show_detail', 'status_color', 'submitted_by']
+                  'formatted_tokens_received', 'answer_files', 'show_answer', 'show_detail', 'status_color',
+                  'submitted_by']
 
     question = QuestionSerializer()
 
