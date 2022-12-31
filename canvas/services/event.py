707fa47b6bcfcbb81e08bd243b1872a1d9cc1ae3
Keyvan <keyvankhademi@gmail.com>
2022-12-30 18:48:26 -0800

    improve event stats

--- canvas/services/event.py
+++ canvas/services/event.py
@@ -1,22 +1,62 @@
 from course.models.models import Submission
 
 
+def _get_status_messages(submissions):
+    status_messages = {}
+
+    for s in submissions.all():
+        if hasattr(s, "get_status_message"):
+            message = s.get_status_message()
+            if message not in status_messages:
+                status_messages[message] = 0
+            status_messages[message] += 1
+
+    return status_messages
+
+
+def _get_error_messages(submissions):
+    error_messages = {}
+
+    for s in submissions.all():
+        if hasattr(s, "get_failed_test_results"):
+            results = s.get_failed_test_results()
+            for result in results:
+                message = result["message"]
+                if message not in error_messages:
+                    error_messages[message] = 0
+                error_messages[message] += 1
+
+    return error_messages
+
+
+def _get_submission_status(submissions):
+    return {
+        "Correct": submissions.filter(is_correct=True).count(),
+        "Partially Correct": submissions.filter(is_correct=False, is_partially_correct=True).count(),
+        "Incorrect": submissions.filter(is_correct=False, is_partially_correct=False).count(),
+    }
+
+
 def get_question_stats(question):
-    submissions = Submission.objects.filter(uqj__question=question).values("answer")
-    choices = question.choices
+    submissions = Submission.objects.filter(uqj__question=question)
 
     answers = {}
-    for submission in submissions:
-        answer = choices[submission["answer"]]
-        if answer not in answers:
-            answers[answer] = 0
-        answers[answer] += 1
+    if hasattr(question, "choices"):
+        choices = question.choices
+        for submission in submissions:
+            answer = choices[submission.answer]
+            if answer not in answers:
+                answers[answer] = 0
+            answers[answer] += 1
 
     return {
         "question": {
             "title": question.title,
         },
         "answers": answers,
+        "error_messages": _get_error_messages(submissions),
+        "submissions": _get_submission_status(submissions),
+        "status_messages": _get_status_messages(submissions),
     }
 
 
