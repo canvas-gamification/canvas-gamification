4bfad4711f876a070f0e2d5d8ae94236f54d730c
yuqi88 <snow34545@gmail.com>
2023-02-08 18:18:56 -0800

    remove category from filter

--- course/services/question.py
+++ course/services/question.py
@@ -43,7 +43,7 @@ def get_number_of_questions_counted_by_category_and_difficulty():
 
     for category in categories:
         for difficulty, _ in DIFFICULTY_CHOICES:
-            available_questions = category.question_set.filter(category=category, difficulty=difficulty).count()
+            available_questions = category.question_set.filter(difficulty=difficulty).count()
 
             result.append(
                 {"category": category.id, "difficulty": difficulty, "available_questions": available_questions}
