ed4142343fde9d5de0dee5e6916878df0b1860d5
Keyvan <keyvankhademi@gmail.com>
2023-02-13 14:09:24 -0800

    add aggregated bugs in goal stats

--- canvas/services/goal.py
+++ canvas/services/goal.py
@@ -1,6 +1,3 @@
-# import distance
-# from sklearn.cluster import AffinityPropagation
-# import numpy as np
 from django.contrib.contenttypes.models import ContentType
 
 from course.models.java import JavaQuestion
@@ -9,72 +6,27 @@ from course.models.multiple_choice import MultipleChoiceQuestion
 from course.models.parsons import ParsonsQuestion
 
 
-def levenshtein(texts):
-    texts = np.asarray(texts, dtype=object)
-    similarity = np.array([[distance.levenshtein(list(w1), list(w2)) for w1 in texts] for w2 in texts])
-    similarity = -1 * similarity
-    return similarity
+def get_submission_bugs(submissions):
+    all_bugs = {}
+    all_patterns = {}
 
+    for submission in submissions.all():
+        if not hasattr(submission, 'bugs'):
+            continue
+        data = submission.bugs
+        for bug in data['bugs']:
+            if bug['type'] not in all_bugs:
+                all_bugs[bug['type']] = bug
+                all_bugs[bug['type']]['count'] = 0
+            all_bugs[bug['type']]['count'] += 1
+        for pattern in data['patterns']:
+            if pattern['type'] not in all_patterns:
+                all_patterns[pattern['type']] = pattern
 
-def text_clustering(texts):
-    texts = [t.split() for t in texts]
-    similarity = levenshtein(texts)
-    affprop = AffinityPropagation(
-        affinity="precomputed", damping=0.5, verbose=False, random_state=0, max_iter=1_000, convergence_iter=10
-    )
-    affprop.fit(similarity)
-    return affprop
-
-
-def cluster_texts(texts):
-    affprop = text_clustering(texts)
-    labels = np.unique(affprop.labels_)
-    texts = np.asarray(texts)
-    clusters = []
-
-    for cluster_id in labels:
-        cluster = np.unique(texts[np.nonzero(affprop.labels_ == cluster_id)])
-        clusters.append(
-            {
-                "exemplar": texts[affprop.cluster_centers_indices_[cluster_id]],
-                "count": len(cluster),
-                "cluster": cluster,
-            }
-        )
-
-    return clusters
-
-
-def cluster_texts_2(texts):
-    return list(set(texts))
-
-
-def get_status_messages(submissions):
-    status_messages = {}
-
-    for s in submissions.all():
-        if hasattr(s, "get_status_message"):
-            message = s.get_status_message()
-            if message not in status_messages:
-                status_messages[message] = 0
-            status_messages[message] += 1
-
-    return status_messages
-
-
-def get_error_messages(submissions):
-    error_messages = []
-
-    for s in submissions.all():
-        if hasattr(s, "get_failed_test_results"):
-            results = s.get_failed_test_results()
-            for result in results:
-                error_messages.append(result["message"])
-
-    if len(error_messages) == 0:
-        return []
-
-    return cluster_texts_2(error_messages)
+    return {
+        'bugs': all_bugs.values(),
+        'patterns': all_patterns.values(),
+    }
 
 
 def get_submission_stats(submissions):
@@ -98,8 +50,7 @@ def get_submission_stats(submissions):
         "total_questions": total_questions,
         "correct_questions": correct_questions,
         "questions_success_rate": 0 if total_questions == 0 else correct_questions / total_questions,
-        "messages": get_status_messages(incorrect_submissions),
-        "error_messages": get_error_messages(incorrect_submissions),
+        "bugs": get_submission_bugs(incorrect_submissions)
     }
 
 
