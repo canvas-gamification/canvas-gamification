a43cb229d980a9d51d69e311fbf00d9efd91a27c
Lydia Lin <60047943+yuqi88@users.noreply.github.com>
2023-02-14 20:21:32 -0800

    Recalcuate leaderboard tokens (#351)
    * include tokens from practice question to leaderboard
    
    * run black
    
    * run black
    
    * correct the filters
    
    * correct the filters
    
    * correct the filters

--- canvas/models/models.py
+++ canvas/models/models.py
@@ -132,6 +132,19 @@ class CanvasCourseRegistration(models.Model):
                 tokens += 0
             else:
                 tokens += event.tokens_received(team)
+
+        from course.models.models import Question
+
+        practiced_uqjs = self.user.question_junctions.filter(
+            user=self.user,
+            question__course=None,
+            question__event=None,
+            question__is_verified=True,
+            question__question_status=Question.CREATED,
+        )
+        for uqj in practiced_uqjs:
+            tokens += uqj.tokens_received
+
         return tokens
 
     @property
