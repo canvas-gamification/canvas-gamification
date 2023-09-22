23fdae211418ac8b15cb373d315a98b8375bdf9d
ketphan02 <tuankietvn@outlook.com>
2023-09-22 16:51:17 -0700

    + Added nickname field in model + Added nickname field to API output for courses and view profile

---
+++ api/error_messages/nickname.py
@@ -0,0 +1,10 @@
+class NICKNAME:
+    REQUIRED = "Nickname is required."
+    INVALID = "Nickname is invalid."
+
+    ERROR_MESSAGES = {
+        "required": REQUIRED,
+        "null": REQUIRED,
+        "blank": REQUIRED,
+        "invalid": INVALID,
+    }
