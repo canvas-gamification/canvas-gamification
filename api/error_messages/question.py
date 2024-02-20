76b3b2ddd6f089ed3c8cff132ffa187fb86ff6a8
Seth Akins <sethlinc4@gmail.com>
2024-02-20 09:50:28 -0800

    Finished validation function

--- api/error_messages/question.py
+++ api/error_messages/question.py
@@ -1,6 +1,8 @@
 class QUESTION:
     REQUIRED = "Question is required."
     INVALID = "Questions is invalid."
+    VARIATION_TYPE_LIST = "Variation types must be a list"
+    INVALID_VARIATION = "Invalid variation type given"
 
     ERROR_MESSAGES = {
         "required": REQUIRED,
